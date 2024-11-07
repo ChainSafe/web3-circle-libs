11ba2e91d2b55b1876a6a5487c04d984c50cb1be
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2024-11-07 10:13:09 -0500

    Update types.ts

--- packages/web3-plugin-circle/src/api/types.ts
+++ packages/web3-plugin-circle/src/api/types.ts
@@ -1,3 +1,7 @@
+/**
+ * Request types
+ */
+
 export type FilterPagesOptions = {
   pageBefore?: string;
   pageAfter?: string;
@@ -8,6 +12,9 @@ export type FilterOptions = {
   to?: string;
 } & FilterPagesOptions;
 
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/create-wallet
+ */
 export type WalletCreateParameters = {
   walletSetId: string;
   blockchains: string[];
@@ -16,6 +23,10 @@ export type WalletCreateParameters = {
   count?: number;
   metadata?: { [key: string]: any }[];
 };
+
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/get-wallets
+ */
 export type WalletListParameters = {
   address?: string;
   blockchain?: string;
@@ -23,12 +34,18 @@ export type WalletListParameters = {
   refId?: string;
 } & FilterOptions;
 
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/update-wallet
+ */
 export type WalletUpdateParameters = {
   id: string;
   name?: string;
   refId?: string;
 };
 
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/list-wallet-balance
+ */
 export type WalletBalanceParameters = {
   id: string;
   includeAll?: boolean;
@@ -37,22 +54,9 @@ export type WalletBalanceParameters = {
   standard?: string;
 } & FilterPagesOptions;
 
-export type Wallet = {
-  id: string;
-  address: string;
-  blockchain: string;
-  createDate: string; // You can also use Date if you prefer to parse it
-  updateDate: string; // Same here
-  custodyType: string; // Use specific value if it’s always this, or allow other potential types
-  name: string;
-  refId: string;
-  state: string; // If the state can only have specific values, you can use string literals like 'LIVE' | 'INACTIVE'
-  userId: string;
-  walletSetId: string;
-  initialPublicKey: string;
-  accountType: string; // Use string literal if there are multiple fixed types
-};
-
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/list-wallet-nfts
+ */
 export type WalletNftsParameters = {
   id: string;
   includeAll?: boolean;
@@ -61,7 +65,14 @@ export type WalletNftsParameters = {
   standard?: string;
 } & FilterPagesOptions;
 
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/get-wallet-sets
+ */
 export type WalletSetListParameters = FilterOptions;
+
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/create-wallet-set
+ */
 export type WalletSetCreateParameters = {
   idempotencyKey?: string;
   name?: string;
@@ -69,11 +80,18 @@ export type WalletSetCreateParameters = {
 export type DeveloperFields = {
   entitySecretCiphertext: string;
 };
+
+/**
+ * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/update-wallet-set
+ */
 export type WalletSetUpdateParameters = {
   id: string;
   name: string;
 };
 
+/**
+ * Response types
+ */
 export type WalletSet = {
   id: string;
   createDate: string;
@@ -115,4 +133,20 @@ export type WalletNft = {
     createDate: string;
   };
   updateDate: string;
+};
+
+export type Wallet = {
+  id: string;
+  address: string;
+  blockchain: string;
+  createDate: string; // You can also use Date if you prefer to parse it
+  updateDate: string; // Same here
+  custodyType: string; // Use specific value if it’s always this, or allow other potential types
+  name: string;
+  refId: string;
+  state: string; // If the state can only have specific values, you can use string literals like 'LIVE' | 'INACTIVE'
+  userId: string;
+  walletSetId: string;
+  initialPublicKey: string;
+  accountType: string; // Use string literal if there are multiple fixed types
 };
