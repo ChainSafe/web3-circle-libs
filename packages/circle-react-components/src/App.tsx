3d8f5917105fc1f403e303b5a3c4d174606c4a14
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-11 13:42:15 +0100

    feat: add tailwind and a few shadcn-ui components

--- packages/circle-react-components/src/App.tsx
+++ packages/circle-react-components/src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 import './App.css';
 
 function App() {
@@ -6,9 +7,9 @@ function App() {
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <h1 className="text-3xl font-bold">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
---
+++ packages/circle-react-components/src/components/NativeTransferForm/NativeTransferForm.tsx
@@ -0,0 +1,26 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
+
+export const NativeTransferForm = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Card Title</CardTitle>
+        <CardDescription>Card Description</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <p>Card Content</p>
+      </CardContent>
+      <CardFooter>
+        <Button>Submit</Button>
+      </CardFooter>
+    </Card>
+  );
+};
