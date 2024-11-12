5546d44ca58ea29fb8a0b106b6e46820bf5569da
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-12 15:27:24 +0100

    feat: add react context and useCircleSdk hook

---
+++ packages/circle-react-components/src/hooks/CircleSdkProvider.tsx
@@ -0,0 +1,20 @@
+import { useMemo, type ReactNode } from 'react';
+import { CircleClient, CircleSdkContext } from './CircleSdkContext';
+
+/**
+ * React component that provides a context for interacting with a shared instance of Circle SDK
+ *
+ * @param children components that may require access to an instance of Circle SDK
+ * @returns React component
+ */
+export function CircleSdkProvider({ children }: { children: ReactNode }) {
+  const context = useMemo(() => {
+    return {
+      client: new CircleClient(),
+    };
+  }, []);
+
+  return (
+    <CircleSdkContext.Provider value={context}>{children}</CircleSdkContext.Provider>
+  );
+}
