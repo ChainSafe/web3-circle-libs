e6ef76076c94e7ffa1ff4cd03f5a9c2eb139df12
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2024-11-26 11:22:32 -0500

    feat: setup remix as a base for demo webapp

---
+++ packages/circle-demo-webapp/app/sdk.ts
@@ -0,0 +1,6 @@
+import { CircleSDK } from '../../web3-circle-sdk';
+
+const apiKey = process.env.CIRCLE_API_KEY as string;
+const secret = process.env.CIRCLE_SECRET as string;
+
+export const sdk = new CircleSDK(apiKey, secret);
