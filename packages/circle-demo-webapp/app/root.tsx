cdbba0a522c9696a8cf41f87c888a029ce511f1b
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-28 10:56:42 +0100

    feat: migrate the components

--- packages/circle-demo-webapp/app/root.tsx
+++ packages/circle-demo-webapp/app/root.tsx
@@ -1,26 +1,18 @@
 import {
   Links,
   Meta,
+  MetaFunction,
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
+} from '@remix-run/react';
+import { Sidebar } from './components/Sidebar';
 
-import "./tailwind.css";
+import './tailwind.css';
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export const meta: MetaFunction = () => {
+  return [{ title: 'Circle SDK Demo' }];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -41,5 +33,13 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <div className="flex h-screen">
+      <Sidebar />
+
+      <div className="flex-1 p-12 overflow-y-auto bg-gray-50">
+        <Outlet />
+      </div>
+    </div>
+  );
 }
