b73b45575926ef0427aad8201be054a9b8146ab0
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-28 13:45:23 +0100

    setup storybook

--- packages/circle-demo-webapp/vite.config.ts
+++ packages/circle-demo-webapp/vite.config.ts
@@ -1,24 +1,35 @@
-import { vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import { vitePlugin as remix } from '@remix-run/dev';
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-declare module "@remix-run/node" {
+declare module '@remix-run/node' {
   interface Future {
     v3_singleFetch: true;
   }
 }
 
+const isStorybook = process.argv[1]?.includes('storybook');
+
 export default defineConfig({
   plugins: [
-    remix({
-      future: {
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_singleFetch: true,
-        v3_lazyRouteDiscovery: true,
-      },
-    }),
+    !isStorybook
+      ? remix({
+          future: {
+            v3_fetcherPersist: true,
+            v3_relativeSplatPath: true,
+            v3_throwAbortReason: true,
+            v3_singleFetch: true,
+            v3_lazyRouteDiscovery: true,
+          },
+        })
+      : react(),
     tsconfigPaths(),
   ],
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './app'),
+    },
+  },
 });
