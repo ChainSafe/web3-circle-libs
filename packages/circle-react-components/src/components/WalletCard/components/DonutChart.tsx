4dd07bcd58bad82b3576ef9dccf01bd168447bdb
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-28 14:28:13 +0100

    remove vite single page app

--- packages/circle-react-components/src/components/WalletCard/components/DonutChart.tsx
+++
@@ -1,9 +0,0 @@
-import pieChart from './pie.svg';
-
-export function DonutChart() {
-  return (
-    <div>
-      <img src={pieChart} alt="Donut Chart" />
-    </div>
-  );
-}
--- packages/circle-react-components/src/hooks/useCircleSdk.ts
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-
-import { CircleSdkContext } from './CircleSdkContext';
-
-export function useCircleSdk() {
-  return useContext(CircleSdkContext);
-}
