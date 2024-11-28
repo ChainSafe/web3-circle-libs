1f798f241ba85cb42293029c58f2a9fe9356484c
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-28 09:30:29 +0100

    remove demo app, move to a separate branch

--- packages/circle-demo-webapp/app/sdk.ts
+++
@@ -1,6 +0,0 @@
-import { CircleSDK } from '../../web3-circle-sdk';
-
-const apiKey = process.env.CIRCLE_API_KEY as string;
-const secret = process.env.CIRCLE_SECRET as string;
-
-export const sdk = new CircleSDK(apiKey, secret);
