1582c9a86f58d1187de09b6cde9d1f7b6c099e50
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-11-28 14:28:13 +0100

    remove vite single page app

--- packages/circle-react-components/src/pages/Overview/components/MenuButton.tsx
+++
@@ -1,9 +0,0 @@
-import { MoreVertical } from 'lucide-react';
-
-export function MenuButton() {
-  return (
-    <button className="text-gray-400 hover:text-gray-600" aria-label="Menu">
-      <MoreVertical size={20} />
-    </button>
-  );
-}
