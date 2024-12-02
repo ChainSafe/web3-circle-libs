22862ff5df76bdb5cb4753005128b4486499b404
Dan Forbes <dan.forbes@chainsafe.io>
2024-12-02 10:29:23 -0500

    chore(docs): workflow
    Add workflow to publish SDK docs and add basic README for the SDK.

--- packages/web3-circle-sdk/src/TokenLookupApi.ts
+++ packages/web3-circle-sdk/src/TokenLookupApi.ts
@@ -6,6 +6,7 @@ export class TokenLookupApi extends BaseApi {
    * Fetches details of a specific token given its unique identifier.
    * Every token in your network of wallets has a UUID associated with it,
    * regardless of whether it's already recognized or was added as a monitored token.
+   * https://developers.circle.com/api-reference/w3s/developer-controlled-wallets/get-token-id
    * @param id The universally unique identifier of the resource.
    * @returns the requested token
    */
