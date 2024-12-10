bc628f44a2f10a7b56bfbc59f5dae6f7811b7e62
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-12-10 15:36:35 +0100

    add wallet set component

---
+++ packages/circle-demo-webapp/app/components/WalletSetDetails/WalletSetDetails.stories.ts
@@ -0,0 +1,28 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { WalletSetDetails } from './WalletSetDetails';
+
+const meta = {
+  title: 'WalletSetDetails',
+  component: WalletSetDetails,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof WalletSetDetails>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    walletSet: {
+      id: 'f270e785-0a7b-578d-a43c-bd514fcc4d49',
+      custodyType: 'DEVELOPER',
+      name: 'Test Wallet Set',
+      updateDate: '2024-11-27T10:14:52Z',
+      createDate: '2024-11-27T10:14:52Z',
+    },
+  },
+};
