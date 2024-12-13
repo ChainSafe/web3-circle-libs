308848a4fcba43d1a1afd9d273de17e519feb102
Kris Urbas <605420+krzysu@users.noreply.github.com>
2024-12-13 17:07:44 +0100

    feat: improve transaction rendering

---
+++ packages/circle-demo-webapp/app/components/TransactionTableHead/TransactionTableHead.tsx
@@ -0,0 +1,13 @@
+export function TransactionTableHead() {
+  return (
+    <thead>
+      <tr className="text-gray-900 text-left text-sm">
+        <th className="py-2">Event</th>
+        <th className="px-4 py-2">From</th>
+        <th className="px-4 py-2">To</th>
+        <th className="px-4 py-2 text-right">Amount</th>
+        <th className="py-2 text-right">Date</th>
+      </tr>
+    </thead>
+  );
+}
