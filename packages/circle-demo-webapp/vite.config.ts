491a4350246b7d194de7ec4fff60bc58f0b9bcf9
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2025-01-28 19:43:42 -0500

    Update vite.config.ts

--- packages/circle-demo-webapp/vite.config.ts
+++ packages/circle-demo-webapp/vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     host: '0.0.0.0',
     port: 3000,
-    allowedHosts: ['web-3-circle-libs-chainsafe.replit.app'],
+    allowedHosts: true,
   },
   plugins: [
     remix({
